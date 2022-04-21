79428918746a2e5aeed7a748e1426cf81fb311c7
Lukas <Lukas.eriksson@skola.taby.se>
2022-04-21 13:16:20 +0200

    start och keypress

--- canvas.js
+++ canvas.js
@@ -1,12 +1,33 @@
-var  canvas = document.getElementById("mycanvas")
-var ctx =  canvas.getContext("2d")
-var grd = ctx.createLinearGradient(0, 0, 5, 13);
-grd.addColorStop(0, "blue");
-grd.addColorStop(1, "yellow");
+let startButton = document.getElementById("start-button")
+let  canvas = document.getElementById("mycanvas")
 
-ctx.fillStyle = grd;
-ctx.fillRect (0,0,20,20)
-ctx.fill()
+let cubeSize = 20
+let xPosCube = 0
+let yPosCube = canvas.height - cubeSize
 
 
+function startGame(){
+    let cube =  canvas.getContext("2d")
+    cube.fillStyle = "green";
+    cube.fillRect (xPosCube, yPosCube, cubeSize,cubeSize)
+    cube.gravity = 0.05
 
+}
+
+document.onkeydown = function(event){
+    const key = event.key
+    if(key === " "){
+        console.log("space")
+        jump()
+    }
+}
+
+function jump(){
+    //hopp funktion 
+}
+
+
+
+
+
+startGame()
